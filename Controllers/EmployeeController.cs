15a5d520203a3710748d2a36c866dce0ffe5926a
RenukaYamala <renuka.yamala@neudesic.com>
2018-06-08 09:26:04 +0530

    First time commits

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,101 @@
+﻿using Employee_ADO.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Employee_ADO.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Employee")]
+    public class EmployeeController : Controller
+    {
+        private string connectionString = "Server=RYAMALA01;database=Employee;Trusted_Connection=True;MultipleActiveResultSets=true";
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            List<Employee> lstemployee = new List<Employee>();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllEmployees", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+
+                    employee.ID = Convert.ToInt32(rdr["EmployeeID"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employee.Gender = rdr["Gender"].ToString();
+                    employee.Department = rdr["Department"].ToString();
+                    employee.City = rdr["City"].ToString();
+
+                    lstemployee.Add(employee);
+                }
+                con.Close();
+            }
+            return lstemployee;
+        }
+        [HttpPost]
+        public void AddEmployee([FromBody]Employee employee)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spAddEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Name", employee.Name);
+                cmd.Parameters.AddWithValue("@Gender", employee.Gender);
+                cmd.Parameters.AddWithValue("@Department", employee.Department);
+                cmd.Parameters.AddWithValue("@City", employee.City);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        [HttpPut]
+        public void UpdateEmployee([FromBody]Employee employee)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spUpdateEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@EmpId", employee.ID);
+                cmd.Parameters.AddWithValue("@Name", employee.Name);
+                cmd.Parameters.AddWithValue("@Gender", employee.Gender);
+                cmd.Parameters.AddWithValue("@Department", employee.Department);
+                cmd.Parameters.AddWithValue("@City", employee.City);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        [HttpDelete("{id}")]
+        public void DeleteEmployee(int id)
+        {
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spDeleteEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@EmpId", id);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+    }
+}
